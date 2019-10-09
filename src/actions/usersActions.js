511f167df698c2d8b42cc986c1044190c23a2dd3
cristhian ochoa <cristhho@gmail.com>
2019-10-09 00:27:18 -0500

    action types

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+import {FETCH_USERS} from '../action-types/usersTypes';
+
 export const fetchAll = () => async (dispatch) => {
 	const users = await axios.get("https://jsonplaceholder.typicode.com/users");
 	dispatch({
-		type: 'fetch_users',
+		type: FETCH_USERS,
 		payload: users.data
 	})
 }
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,10 +1,12 @@
+import {FETCH_USERS} from '../action-types/usersTypes';
+
 const INITIAL_STATE = {
 	usuarios: []
 };
 
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
-		case 'fetch_users':
+		case FETCH_USERS:
 			return {...state, usuarios: action.payload};
 		default:
 			return state;
